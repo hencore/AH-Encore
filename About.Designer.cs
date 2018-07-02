dba53e9ad43441d3dddf170aa15075ab3e2dbf92
hencore <40734896+hencore@users.noreply.github.com>
2018-07-02 15:16:03 +0300

    Add files via upload

---
+++ About.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace auto_h_encore
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 107);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Мой профиль 4PDA:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.Location = new System.Drawing.Point(167, 130);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(81, 17);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "nikolay2002";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(12, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Мой GitHub:";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.linkLabel2.Location = new System.Drawing.Point(167, 159);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(57, 17);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "hencore";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "noahc3 GitHub";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.linkLabel3.Location = new System.Drawing.Point(167, 186);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(52, 17);
+            this.linkLabel3.TabIndex = 6;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "noahc3";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 225);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "About";
+            this.Text = "About";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.About_FormClosed);
+            this.Load += new System.EventHandler(this.About_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace auto_h_encore {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+            VerifyUserInfo();
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            lblVersion.Text = "ah-encore by nikolay2002 and noahc3  v." + Reference.version;
+            lblInfo.Text = "FAQ: \r\n1. Установите и откройте QCMA (щелкнув на эти строчки)\r\n2. Подключите Вашу PS Vita к PC и запустите на ней \"Управление данными\"\r\n3. Выберите \"Скопировать данные\", после Компьютер -> Система PS Vita - Приложения - PS Vita\r\n4. Теперь в этой программе нажмите на \"Проверить наличие QCMA\" и после - на \"Start\"\r\n5. Дождитесь окончания работы программы, после на вашей PS Vita нажмите стрелочку назад и снова откройте вкладку \"PS Vita\", поставьте галочку на H-Encore и нажмите Копировать\r\n7.Теперь откройте H-Encore на PS Vita, после чего установите HENkaku и VitaShell\r\n8. После каждой перезагрузки PS Vita открывайте H-Encore и нажимайте Exit";
+        }
+
+        private void VerifyUserInfo() {
+            Reference.app_PATH = (string)Registry.CurrentUser.OpenSubKey("Software\\codestation\\qcma").GetValue("appsPath", "");
+            if (Directory.Exists(Reference.app_PATH + "\\APP\\"))
+            {
+                set_AID();
+                info("QCMA найден, можете начинать установку! (начинайте с пункта 4)");
+                btnStart.Enabled = true;
+            }
+            else btnStart.Enabled = false;
+        }
+
+        private void generateDirectories(string AID) {
+            info("Generating working directories...");
+            if (Directory.Exists(Reference.path_data)) Directory.Delete(Reference.path_data, true);
+            if (Directory.Exists(Reference.app_PATH + "\\APP\\" + AID + "\\PCSG90096\\")) {
+                if (MessageBox.Show("Вы должны удалить старый бекап BitterSmile из вашей QCMA директории. Удалить?", "Внимание", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                    Directory.Delete(Reference.app_PATH + "\\APP\\" + Reference.AID + "\\PCSG90096\\", true);
+                } else {
+                    throw new IOException("Directory Already Exists");
+                }
+            }
+            Directory.CreateDirectory(Reference.path_data);
+            Directory.CreateDirectory(Reference.path_hencore);
+            Directory.CreateDirectory(Reference.path_psvimgtools);
+            Directory.CreateDirectory(Reference.path_pkg2zip);
+            Directory.CreateDirectory(Reference.path_downloads);
+            incrementProgress();
+        }
+
+        private void downloadFiles() {
+            Utility.DownloadFile(this, true, Reference.url_hencore, Reference.path_downloads + "hencore.zip");
+            Utility.ExtractFile(this, true, Reference.path_downloads + "hencore.zip", Reference.path_hencore);
+            
+            Utility.DownloadFile(this, true, Reference.url_psvimgtools, Reference.path_downloads + "psvimgtools.zip");
+            Utility.ExtractFile(this, true, Reference.path_downloads + "psvimgtools.zip", Reference.path_psvimgtools);
+            
+            Utility.DownloadFile(this, true, Reference.url_pkg2zip, Reference.path_downloads + "pkg2zip.zip");
+            Utility.ExtractFile(this, true, Reference.path_downloads + "pkg2zip.zip", Reference.path_pkg2zip);
+            
+            Utility.DownloadFile(this, true, Reference.url_bittersmile, Reference.path_downloads + "bittersmile.pkg");
+        }
+
+        private void PackageHencore(string encKey) {
+            Utility.PackageFiles(this, true, Reference.path_hencore + "h-encore\\", encKey, "app");
+            Utility.PackageFiles(this, true, Reference.path_hencore + "h-encore\\", encKey, "appmeta");
+            Utility.PackageFiles(this, true, Reference.path_hencore + "h-encore\\", encKey, "license");
+            Utility.PackageFiles(this, true, Reference.path_hencore + "h-encore\\", encKey, "savedata");
+        }
+
+
+        private void toggleControls(bool state) {
+            if (InvokeRequired) {
+                Invoke(new Action(() => {
+                    btnStart.Enabled = state;                   
+                    barProgress.Value = 0;
+                }));
+            } else {
+                btnStart.Enabled = state;
+                barProgress.Value = 0;
+            }
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e) {
+            chk_QCMA.Enabled = false;
+            toggleControls(false);
+
+            //run code on new thread to keep UI responsive
+            Task.Factory.StartNew(new Action(() => {
+
+                try {
+                    generateDirectories(Reference.AID);
+                    downloadFiles();
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                    toggleControls(true);
+                    return;
+                }
+                
+                try {
+                    info("Распаковка BitterSmile с помощью pkg2zip...");
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.WorkingDirectory = Reference.path_pkg2zip;
+                    psi.Arguments = "-x \"" + Reference.path_downloads + "bittersmile.pkg\"";
+                    psi.FileName = Reference.path_pkg2zip + "pkg2zip.exe";
+                    Process process = Process.Start(psi);
+                    process.WaitForExit();
+                    info("      Done!");
+                    incrementProgress();
+                } catch (FileNotFoundException) {
+                    MessageBox.Show("Похоже, скачанные файлы исчезли. Пожалуйста, перезапустите приложение и не трогайте файлы в его директориях");
+                    toggleControls(true);
+                    return;
+                }
+                
+                try {
+                    foreach (string k in Directory.EnumerateFiles(Reference.path_pkg2zip + "app\\PCSG90096\\")) {
+                        info("Перемещение " + k.Split('\\').Last() + " в рабочую директорию h-encore...");
+                        FileSystem.MoveFile(k, Reference.path_hencore + "\\h-encore\\app\\ux0_temp_game_PCSG90096_app_PCSG90096\\" + k.Split('\\').Last());
+                    }
+                } catch (DirectoryNotFoundException) {
+                    MessageBox.Show("Похоже, созданые файлы исчезли. Пожалуйста, перезапустите приложение и не трогайте файлы в его директориях");
+                    toggleControls(true);
+                    return;
+                } catch (UnauthorizedAccessException) {
+                    MessageBox.Show("Приложение не имеет прав для записи в эту директорию. Пожалуйста, переместите его в ту, в которой у вас такие права будут, или перезапустите приложение от имени администратора");
+                    toggleControls(true);
+                    return;
+                } catch (IOException ex) {
+                    MessageBox.Show("Что-то пошло не так:\r\n\r\n" + ex.Message);
+                    toggleControls(true);
+                    return;
+                }
+
+                try {
+                    foreach (string k in Directory.EnumerateDirectories(Reference.path_pkg2zip + "app\\PCSG90096\\")) {
+                        info("Перемещение " + k.Split('\\').Last() + " в рабочую директорию h-encore...");
+                        FileSystem.MoveDirectory(k, Reference.path_hencore + "\\h-encore\\app\\ux0_temp_game_PCSG90096_app_PCSG90096\\" + k.Split('\\').Last());
+                    }
+                } catch (DirectoryNotFoundException) {
+                    MessageBox.Show("Похоже, созданые файлы исчезли. Пожалуйста, перезапустите приложение и не трогайте файлы в его директориях");
+                    toggleControls(true);
+                    return;
+                } catch (UnauthorizedAccessException) {
+                    MessageBox.Show("Приложение не имеет прав для записи в эту директорию. Пожалуйста, переместите его в ту, в которой у вас такие права будут, или перезапустите приложение от имени администратора");
+                    toggleControls(true);
+                    return;
+                } catch (IOException ex) {
+                    MessageBox.Show("Что-то пошло не так:\r\n\r\n" + ex.Message);
+                    toggleControls(true);
+                    return;
+                }
+
+                incrementProgress();
+
+                try {
+                    info("Перемещение файла лицензии...");
+                    FileSystem.MoveFile(Reference.path_hencore + "\\h-encore\\app\\ux0_temp_game_PCSG90096_app_PCSG90096\\sce_sys\\package\\temp.bin", Reference.path_hencore + "\\h-encore\\license\\ux0_temp_game_PCSG90096_license_app_PCSG90096\\6488b73b912a753a492e2714e9b38bc7.rif");
+                    info("      Done!");
+                    incrementProgress();
+                } catch (DirectoryNotFoundException) {
+                    MessageBox.Show("Похоже, созданые файлы исчезли. Пожалуйста, перезапустите приложение и не трогайте файлы в его директориях");
+                    toggleControls(true);
+                    return;
+                } catch (UnauthorizedAccessException) {
+                    MessageBox.Show("Приложение не имеет прав для записи в эту директорию. Пожалуйста, переместите его в ту, в которой у вас такие права будут, или перезапустите приложение от имени администратора");
+                    toggleControls(true);
+                    return;
+                } catch (IOException ex) {
+                    MessageBox.Show("Что-то пошло не так:\r\n\r\n" + ex.Message);
+                    toggleControls(true);
+                    return;
+                }
+
+                string encKey;
+
+                try {
+                    info("Получение ключа расшифровки CMA с использованием вашего AID " + Reference.AID);
+                    encKey = Utility.GetEncKey(Reference.AID);
+                    if (encKey.Length != 64) return;
+                    info("Ключ получен " + encKey);
+                    incrementProgress();
+                } catch (Exception) {
+                    toggleControls(true);
+                    return;
+                }
+                
+                try {
+                    PackageHencore(encKey);
+                } catch (Exception) {
+                    toggleControls(true);
+                    return;
+                }
+
+                try {
+                    info("Перемещение h-encore файлов в QCMA APP директорию...\r\n");
+                    FileSystem.MoveDirectory(Reference.path_hencore + "h-encore\\PCSG90096\\", Reference.app_PATH + "\\APP\\" + Reference.AID + "\\PCSG90096\\");
+                    incrementProgress();
+                    info("Готово!! ");
+                } catch (DirectoryNotFoundException) {
+                    MessageBox.Show("Ваша директория QCMA исчезла! Убедитесь, что она не была случайно удалена!");
+                    toggleControls(true);
+                    return;
+                } catch (UnauthorizedAccessException) {
+                    MessageBox.Show("Приложение не имеет прав для записи в директорию QCMA. Пожалуйста, перезапустите приложение от имени администратора");
+                    toggleControls(true);
+                    return;
+                } catch (IOException ex) {
+                    MessageBox.Show("Что-то пошло не так:\r\n\r\n" + ex.Message);
+                    toggleControls(true);
+                    return;
+                }
+
+
+                toggleControls(true);
+            }));
+            
+        }
+
+        public void incrementProgress() {
+            if (InvokeRequired) Invoke(new Action(() => barProgress.Value++));
+            else barProgress.Value++;
+        }
+
+        public void info(string message) {
+            if (InvokeRequired) Invoke(new Action(() => txtLog.AppendText("[" + DateTime.Now.ToLongTimeString() + "] " + message + "\r\n")));
+            else txtLog.AppendText("[" + DateTime.Now.ToLongTimeString() + "] " + message + "\r\n");
+
+        }
+
+        private void set_AID()
+        {
+            DirectoryInfo inf = new DirectoryInfo(Reference.app_PATH + "\\APP\\");
+            DirectoryInfo[] dirs = inf.GetDirectories();
+            if (dirs.Length != 0 && dirs[0].Name.Length == 16)
+            {
+                Reference.AID = dirs[0].Name;
+            }
+        }
+
+        private void chk_QCMA_Click(object sender, EventArgs e)
+        {
+            VerifyUserInfo();
+        }
+
+        private void bt_About_Click(object sender, EventArgs e)
+        {
+            Form ab = new About();
+            ab.Show();
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://codestation.github.io/qcma/");
+        }
+    }
+}
